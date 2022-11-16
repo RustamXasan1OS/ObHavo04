8ad2390e21f03526a8f05429e9e6ea4572c7e980
RustamXasan1OS <rustamxasan1@gmail.com>
2022-11-16 01:42:52 -0800

    04StackView

--- ObHavoApp04/WatherViewController.swift
+++ ObHavoApp04/WatherViewController.swift
@@ -8,17 +8,26 @@
 import UIKit
 
 class WatherViewController: UIViewController {
-
+    
     let backgroundView = UIImageView()
     
-    //01 joylashuv iconkasini elon qildik
+    //06
+    let rootStackView = UIStackView()
+    
+    //01
+    let searchStackView = UIStackView()
+    //search
     let locationButton = UIButton()
-    
-    //05 qidiruv iconkasini elon qildik
     let searchButton = UIButton()
-    
-    //09 qidruv uchun TexField
     let searchTextField = UITextField()
+    
+    //11 weather
+    let conditationImageView = UIImageView()
+    //15
+    let temperatureLabel = UILabel()
+    let cityLabel  = UILabel()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,36 +44,75 @@ extension WatherViewController {
         backgroundView.image = UIImage(named: "background")
         backgroundView.contentMode = .scaleAspectFill
         
-        //02
+        //07
+        rootStackView.translatesAutoresizingMaskIntoConstraints = false
+        rootStackView.axis = .vertical
+        rootStackView.alignment = .trailing
+        rootStackView.spacing = 10
+        
+        //02 search
+        searchStackView.translatesAutoresizingMaskIntoConstraints = false
+        searchStackView.spacing = 8
+        
         locationButton.translatesAutoresizingMaskIntoConstraints = false
         locationButton.setBackgroundImage(UIImage(systemName: "location.viewfinder"), for: .normal)
         locationButton.tintColor = .label
         
-        //06
         searchButton.translatesAutoresizingMaskIntoConstraints = false
         searchButton.setBackgroundImage(UIImage(systemName: "magnifyingglass"), for: .normal)
         searchButton.tintColor = .label
         
-        //10
         searchTextField.translatesAutoresizingMaskIntoConstraints = false
         searchTextField.font = UIFont.preferredFont(forTextStyle: .title1)
         searchTextField.placeholder = "Search"
         searchTextField.textAlignment = .right
         searchTextField.borderStyle = .roundedRect
         searchTextField.backgroundColor  = .systemFill
+        
+        //12 weather
+        conditationImageView.translatesAutoresizingMaskIntoConstraints = false
+        conditationImageView.image = UIImage(systemName: "sun.max.fill")
+        conditationImageView.tintColor = .systemYellow
+        
+        //16
+        temperatureLabel.translatesAutoresizingMaskIntoConstraints = false
+        temperatureLabel.font = UIFont.systemFont(ofSize: 50)
+        temperatureLabel.text = "15°C"
+        temperatureLabel.textColor = .systemOrange
+        
+        
+        cityLabel.translatesAutoresizingMaskIntoConstraints = false
+        cityLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
+        cityLabel.text = "TashkentCity"
+        cityLabel.textColor = .systemBlue   
+        
     }
     
     func layout() {
         
         view.addSubview(backgroundView)
-        //03
-        view.addSubview(locationButton)
+        
+//        //05
+//        view.addSubview(searchStackView)
         
         //08
-        view.addSubview(searchButton)
+        view.addSubview(rootStackView)
         
-        //11
-        view.addSubview(searchTextField)
+        //09
+        rootStackView.addArrangedSubview(searchStackView)
+        
+        //13
+        rootStackView.addArrangedSubview(conditationImageView)
+        
+        //17
+        rootStackView.addArrangedSubview(temperatureLabel)
+        rootStackView.addArrangedSubview(cityLabel)
+        
+        //03
+        searchStackView.addArrangedSubview(locationButton)
+        
+        searchStackView.addArrangedSubview(searchTextField)
+        searchStackView.addArrangedSubview(searchButton)
         
         
         //AutoLayout
@@ -74,26 +122,29 @@ extension WatherViewController {
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             
-            //04 Location iconkani ekranga chiqarish
-            locationButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            locationButton.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            //10
+            rootStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            rootStackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: rootStackView.trailingAnchor, multiplier: 1),
+            
+            
+            //04
+            searchStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            searchStackView.leadingAnchor.constraint(equalToSystemSpacingAfter:view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: rootStackView.trailingAnchor, multiplier: 1),
+            searchStackView.widthAnchor.constraint(equalTo: rootStackView.widthAnchor),
+            
+
             locationButton.widthAnchor.constraint(equalToConstant: 40),
             locationButton.heightAnchor.constraint(equalToConstant: 40),
             
-            //07 qidiruv iconca ni qoshamiz
-            searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter:
-                                            searchButton.trailingAnchor, multiplier: 1),
+
             searchButton.widthAnchor.constraint(equalToConstant: 40),
             searchButton.heightAnchor.constraint(equalToConstant: 40),
             
-            //12
-            searchTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            searchTextField.leadingAnchor.constraint(equalToSystemSpacingAfter: locationButton.trailingAnchor, multiplier: 1),
-            searchButton.leadingAnchor.constraint(equalToSystemSpacingAfter: searchTextField.trailingAnchor, multiplier: 1),
-            
-            
+            //14
+            conditationImageView.heightAnchor.constraint(equalToConstant: 120),
+            conditationImageView.widthAnchor.constraint(equalToConstant: 120),
             
         ])
     }
